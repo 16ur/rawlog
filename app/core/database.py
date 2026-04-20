88dfc6e3f900bb964debc596622493a12636f41b
16ur <akuseruuu@protonmail.com>
2026-04-20 22:52:40 +0200

    fix(database): add valid error handling for database_url

--- app/core/database.py
+++ app/core/database.py
@@ -6,7 +6,9 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "axel.fr")
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL environment variable not set")
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
