cdeb00532c642a3ff504ca36edbfe9dde5551ae3
16ur <akuseruuu@protonmail.com>
2026-04-20 23:03:19 +0200

    feat(main): add middleware config for cors

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.api.v1 import workouts
 from app.api.v1 import exercises
 from app.api.v1 import muscles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="rawlog API")
 
@@ -9,6 +10,13 @@ app = FastAPI(title="rawlog API")
 app.include_router(workouts.router, prefix="/api/v1", tags=["workouts"])
 app.include_router(exercises.router, prefix="/api/v1", tags=["exercises"])
 app.include_router(muscles.router, prefix="/api/v1", tags=["muscles"])
+app.add_middleware(
+    CORSMiddleware,  #  type: ignore[arg-type]
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
